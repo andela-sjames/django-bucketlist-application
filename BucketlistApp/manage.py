8e11a2c50abed3b66a0e4ce4048decded57bb12e
Samuel James <samuel.james@andela.com>
2015-11-13 07:19:23 +0100

    add travis file for test coverage

--- BucketlistApp/manage.py
+++ BucketlistApp/manage.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
 
     from django.core.management import execute_from_command_line
     args = [argument.decode('UTF-8') for argument in sys.argv]
-    execute_from_command_line(sys.argv)
+    execute_from_command_line(args)
