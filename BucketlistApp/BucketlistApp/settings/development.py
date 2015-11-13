e0f74462dea7089510edaab8122c385e92c463f6
Samuel James <samuel.james@andela.com>
2015-11-13 23:48:29 +0100

    add space

--- BucketlistApp/BucketlistApp/settings/development.py
+++ BucketlistApp/BucketlistApp/settings/development.py
@@ -8,7 +8,6 @@ from .base import *
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
