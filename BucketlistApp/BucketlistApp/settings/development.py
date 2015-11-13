b1faabaa15310111bf48486780b01bd93e0b20bd
Samuel James <samuel.james@andela.com>
2015-11-13 23:26:22 +0100

    make change to seetings modules

--- BucketlistApp/BucketlistApp/settings/development.py
+++ BucketlistApp/BucketlistApp/settings/development.py
@@ -8,6 +8,7 @@ from .base import *
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
