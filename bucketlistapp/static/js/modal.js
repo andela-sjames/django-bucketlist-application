e921ea0a9424d32909bb0d6adc7f2748babe83a2
Samuel James <samuel.james@andela.com>
2016-01-11 22:47:43 +0100

    modify sigup page to use bootstrap-material-design

--- bucketlistapp/static/js/modal.js
+++ bucketlistapp/static/js/modal.js
@@ -24,23 +24,26 @@ var deleteBuckelist = {
    }
 };
 
+
 $(document).ready(function(){
     deleteBuckelist.init({
         button: '.glyphicon-trash'
     })
+  $.material.init();
 
-    $('#tool').tooltip(); 
+
+    $('#tool').tooltip();
     $( "#editme" ).hide( "fast" )
 
     $( ".bucket-item" ).find('.edit').click(function() {
           var parent = $(this).parentsUntil('.bucket-item').parent();
-          
+
           parent.find('.edit-form').slideToggle('fast');
     });
-      
+
     $('#additemmodal').on('show.bs.modal', function (event) {
           var button = $(event.relatedTarget) // Button that triggered the modal
-          var bucketlistId = button.data('bucketlistId') 
+          var bucketlistId = button.data('bucketlistId')
           var modalForm = $(this).find('form')
           var modalFormAction = "/bucketlist/" + bucketlistId + "/items/"
 
@@ -49,7 +52,7 @@ $(document).ready(function(){
 
     $('#editmodal').on('show.bs.modal', function (event) {
           var button = $(event.relatedTarget) // Button that triggered the modal
-          var bucketlistId = button.data('bucketlistId') 
+          var bucketlistId = button.data('bucketlistId')
           var modalForm = $(this).find('form')
           var modalFormAction = "/bucketlist/" + bucketlistId + "/"
 
